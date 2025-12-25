2d6c735dfbfb344a1a8ce97e061ef5866bdc694b
20850-dev <20850@kanu.ac.th>
2025-12-25 20:26:43 +0700

    Update route.ts

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const BOT_TOKEN = '7734824066:AAGQ0Z0VRVbnip8CNgBVaU7ywAa8gzSHfOQ';
-const CHAT_ID = '6178198460';
+const BOT_TOKEN = '7671148552:AAH2o1Be50-P4OlhRv8y_QsmUcnEyEpHOXc';
+const CHAT_ID = '-3142534544';
 
 const POST = async (request: NextRequest): Promise<NextResponse> => {
     try {
